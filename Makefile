1b1e24f0fab28338a05081c4b9ce5042a013b189
b <b@DESKTOP-A3RKR60>
2023-01-19 16:08:41 -0600

    intial commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+CFLAGS = -I C:\ffmpeg\include
+LDFLAGS = -L C:\ffmpeg\lib -lavcodec -lavformat -lavutil
+
+all:
+	g++ $(CFLAGS) -o main.exe main.cpp $(LDFLAGS)
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+
+
+extern "C" {
+    #include <libavcodec/avcodec.h>
+    #include <libavformat/avformat.h>
+    #include <libavutil/avutil.h>
+    #include <libswscale/swscale.h>
+}
+
+
+int main(int arc, char** argv) {
+    return 0;
+}
